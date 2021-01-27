ede03669217448eaf09a48ad0203dc37a0eba8be
Matheus <76071892+iboy-exploitado@users.noreply.github.com>
2021-01-27 17:42:45 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import os 
+import requests
+os.system("color a")
+os.system("cls")
+print(f'''
+
+  ██████╗ ██████╗  ███╗  ██╗███████╗██╗  ██╗██╗  ████████╗ █████╗      ██████╗███████╗██████╗ 
+██╔════╝██╔═══██╗████╗  ██║██╔════╝██║   ██║██║  ╚══██╔══╝██╔══██╗    ██╔════╝██╔════╝██╔══██╗
+██║     ██║   ██║██╔██╗ ██║███████╗██║   ██║██║     ██║   ███████║    ██║     █████╗  ██████╔╝
+██║     ██║   ██║██║╚██╗██║╚════██║██║   ██║██║     ██║   ██╔══██║    ██║     ██╔══╝  ██╔═══╝ 
+╚██████╗╚██████╔╝██║ ╚████║███████║╚██████╔╝███████╗██║   ██║  ██║    ╚██████╗███████╗██║     
+ ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚══════╝╚═╝   ╚═╝  ╚═╝     ╚═════╝╚══════╝╚═╝     
+
+
+######################################
+####  Coded by: Iboy-Exploitado   #### 
+######################################
+
+
+   ''')
+
+cep = input("Insira o CEP: ")
+reques = requests.get(f"https://viacep.com.br/ws/{cep}/json/")
+json = reques.json()
+print(f"""
+
+CEP: {cep}
+Logradouro: {json['logradouro']}
+Complemento: {json['complemento']}
+Bairro: {json['bairro']}
+Localidade: {json['localidade']}
+Estado: {json['uf']}
+Ibge: {json['ibge']}
+Gia: {json['gia']}
+DDD: {json['ddd']}
+Siafi: {json['siafi']}
+
+	""")
+
+
